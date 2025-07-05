79cf804de4472437e88c8c4da95220e6b6c0aa00
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-07-06 02:00:42 +0700

    feat: Them chuc nang crud user, phan quyen, xac thuc admin

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
+const { authMiddleware, isAdmin } = require("../middleware/authMiddleWare");
 const {
   createUser,
   loginUserController,
+  getAllUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+  blockUser,
+  unlockUser,
 } = require("../controllers/userController");
 
 router.post("/register", createUser);
 router.get("/login", loginUserController);
+router.get("/all-users", getAllUsers);
+router.get("/:id",authMiddleware,isAdmin,getUser);
+router.delete("/:id", deleteUser);
+router.put("/update-user",authMiddleware,isAdmin, updateUser);
+router.put("/block-user/:id", authMiddleware, blockUser);
+router.put("/unlock-user/:id", authMiddleware, unlockUser);
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
