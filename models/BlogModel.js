d1ad3aead65601992d84efc602a03e991c4674c4
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-08-13 23:52:31 +0700

    your commit message

--- models/BlogModel.js
+++ models/BlogModel.js
@@ -1,55 +1,64 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var blogSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
+var blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    description:{
-        type:String,
-        required:true,
+    description: {
+      type: String,
+      required: true,
     },
-    category:{
-        type:String,
-        required:true,
+    category: {
+      type: String,
+      required: true,
     },
-    numViews:{
-        type: Number,
-        default: 0
+    numViews: {
+      type: Number,
+      default: 0,
     },
-    isLiked:{
-        type: Boolean,
-        default: false
+    isLiked: {
+      type: Boolean,
+      default: false,
     },
-    isDisliked:{
-        type: Boolean,
-        default: false
+    isDisliked: {
+      type: Boolean,
+      default: false,
     },
-    likes:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }],
-    image:{
-        type:String,
-        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMf8AbBrrC6gbouwEQ8iaDyi3nsdPRoF9EkQ&s"
+    likes: [
+      {
+        type: mongoose.Schema.ObjectId, ref: "User"
+      },
+    ],
+
+    dislikes: [
+      {
+        type: mongoose.Schema.ObjectId, ref: "User"
+      },
+    ],
+
+    image: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMf8AbBrrC6gbouwEQ8iaDyi3nsdPRoF9EkQ&s",
     },
-    author:{
-        type:String,
-        default: "Admin"
+    author: {
+      type: String,
+      default: "Admin",
     },
-    
-},
-{
-    toJSON:{
-        virtuals: true
+  },
+  {
+    toJSON: {
+      virtuals: true,
     },
-    toObject:{
-        virtuals: true
+    toObject: {
+      virtuals: true,
     },
     timestamps: true,
-  
-});
+  }
+);
 
 //Export the model
-module.exports = mongoose.model('Blog', blogSchema);
+module.exports = mongoose.model("Blog", blogSchema);
