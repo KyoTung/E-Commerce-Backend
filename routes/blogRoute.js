89ee7ae7fd88052ea430add79c9e27d9fec912b6
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-08-13 23:52:31 +0700

    your commit message

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const { isAdmin, authMiddleware } = require("../middleware/authMiddleWare");
 
-const {createBlog} = require("../controller/blogController")
+const {
+  createBlog,
+  updateBlog,
+  getAllBlogs,
+  getBlog,
+  deleteBlog,
+  likeBlog,
+  dislikeBlog
+} = require("../controller/blogController");
 
 const router = express.Router();
 
-router.post("/",authMiddleware, isAdmin, createBlog);
+router.post("/", authMiddleware, isAdmin, createBlog);
+router.put("/likes", authMiddleware, likeBlog);
+router.put("/dislikes", authMiddleware, dislikeBlog);
+router.put("/:id", authMiddleware, isAdmin, updateBlog);
+router.get("/:id", getBlog);
+router.get("/", getAllBlogs);
+router.delete("/:id", authMiddleware, isAdmin, deleteBlog);
+
+
+
 
 module.exports = router;
