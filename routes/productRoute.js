739c046266843adc0d27bc0836b47b2eaf1909d3
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-08-21 22:11:34 +0700

    feat: them chuc nang upload anh vao local va cloudinay-hoan thien

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -8,19 +8,30 @@ const {
   deleteProduct,
   addToWishList,
   rating,
+  uploadImagesProduct
 } = require("../controller/productController");
 
+const {
+  uploadPhoto,
+  productImgResize,
+} = require("../middleware/uploadImage");
 
 const router = express.Router();
-
+router.put(
+  "/upload/:id",
+  authMiddleware,
+  isAdmin,
+  uploadPhoto.array("images", 10),
+  productImgResize,
+  uploadImagesProduct
+);
 router.post("/", authMiddleware, isAdmin, createProduct);
 router.get("/:id", getAProduct);
-router.put("/wishlist",authMiddleware, addToWishList);
+router.put("/wishlist", authMiddleware, addToWishList);
 router.put("/rating", authMiddleware, rating);
 
 router.put("/:id", authMiddleware, isAdmin, updateProduct);
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
 router.get("/", getAllProduct);
-
 
 module.exports = router;
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,18 +1,12 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'uploads', // Tên folder trên Cloudinary
-    allowed_formats: ['jpg', 'png', 'jpeg'],
-  },
 });
 
 const cloudinaryUploadImage = async(fileUpload) =>{
