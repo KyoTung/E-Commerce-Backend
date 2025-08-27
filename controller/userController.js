f7f2cb8221d4c6310069fd43e3ef832ea7e3c867
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-08-27 10:35:17 +0700

    feat: them update user infor, get wishlist

--- controller/userController.js
+++ controller/userController.js
@@ -25,10 +25,20 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-// login
-const loginUserController = asyncHandler(async (req, res) => {
+// login user
+const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const findUser = await User.findOne({ email: email });
+
+  if (!findUser) {
+    throw new Error("Account Not Found");
+  }
+
+
+  const isPasswordValid = await findUser.isPasswordMatched(password);
+  if (!isPasswordValid) {
+    throw new Error("Incorrect Password");
+  }
 
   if (findUser && (await findUser.isPasswordMatched(password))) {
     const refreshToken = await generateRefreshToken(findUser?._id);
@@ -54,6 +64,45 @@ const loginUserController = asyncHandler(async (req, res) => {
   } else {
     throw new Error("Invalid Crendentials!");
   }
+});
+
+const loginAdmin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const findAdmin = await User.findOne({ email });
+
+  if (!findAdmin) {
+    throw new Error("Administrator Account Not Found");
+  }
+
+  if (findAdmin.role !== "admin") {
+    throw new Error("No Access");
+  }
+
+  const isPasswordValid = await findAdmin.isPasswordMatched(password);
+  if (!isPasswordValid) {
+    throw new Error("Incorrect Password");
+  }
+
+  const refreshToken = await generateRefreshToken(findAdmin.id);
+  await User.findByIdAndUpdate(
+    findAdmin._id,
+    { refreshToken },
+    { new: true }
+  );
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    maxAge: 72 * 60 * 60 * 1000,
+  });
+
+  res.json({
+    _id: findAdmin._id,
+    fullName: findAdmin.fullName,
+    email: findAdmin.email,
+    address: findAdmin.address,
+    phone: findAdmin.phone,
+    token: generateToken(findAdmin._id),
+  });
 });
 
 const handleRefreshToken = asyncHandler(async (req, res) => {
@@ -141,6 +190,32 @@ const updateUser = asyncHandler(async (req, res) => {
   }
 });
 
+// save address user
+const updateInfo = asyncHandler(async(req, res) =>{
+  const {_id} = req.user;
+    validateMongoDbId(_id);
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        address: req?.body?.address,
+        phone: req?.body?.phone,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({
+      message: "Infomation updated successfully",
+      success: true,
+      user: updateUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+})
+
+
 const blockUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoDbId(id);
@@ -182,7 +257,7 @@ const logout = asyncHandler(async (req, res) => {
     throw new Error("No Refresh Token In Cookies");
   }
   const user = await User.findOne({ refreshToken });
-  if (!user){
+  if (!user) {
     res.clearCookie("refreshToken", {
       httpOnly: true,
       secure: true,
@@ -191,63 +266,62 @@ const logout = asyncHandler(async (req, res) => {
   }
   await User.findOneAndUpdate(
     { refreshToken: refreshToken },
-    { refreshToken: "",}
+    { refreshToken: "" }
   );
   res.clearCookie("refreshToken", {
     httpOnly: true,
     secure: true,
   });
-   res.sendStatus(204)
-    
+  res.sendStatus(204);
 });
 
-const updatePassword = asyncHandler(async (req, res) =>{
-    const {_id} = req.user;
-    const {password} = req.body;
-    validateMongoDbId(_id);
-    const user = await User.findById(_id);
-    if(password){
-      user.password = password;
-      const updatePassword = await user.save();
-      res.json(updatePassword)
-    } else {
-      res.json(user)
-    }
-})
+const updatePassword = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { password } = req.body;
+  validateMongoDbId(_id);
+  const user = await User.findById(_id);
+  if (password) {
+    user.password = password;
+    const updatePassword = await user.save();
+    res.json(updatePassword);
+  } else {
+    res.json(user);
+  }
+});
 
-const forgotPasswordToken = asyncHandler(async(req, res)=>{
-   const {email} = req.body;
-   const user = await User.findOne({email})
-   if(!user){
+
+const forgotPasswordToken = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
     throw new Error("User not found with this email");
-   }
-   try{
-     const token = await user.createResetToken();
-     await user.save();
-     const resetURL = `Hi, Please folow this link to to reset your Password. This link is valid till 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</a>`
-     const data = {
-      to:email,
+  }
+  try {
+    const token = await user.createResetToken();
+    await user.save();
+    const resetURL = `Hi, Please folow this link to to reset your Password. This link is valid till 10 minutes from now. <a href='http://localhost:5000/api/user/reset-password/${token}'>Click Here</a>`;
+    const data = {
+      to: email,
       text: "Hey User",
       subject: "Forgot Password Link",
       html: resetURL,
-     }
-     sendEmail(data)
-     res.json(token)
-   }
-   catch(error){
-    throw new Error(error)
-   }
-})
+    };
+    sendEmail(data);
+    res.json(token);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-const resetPassword = asyncHandler(async(req, res) =>{
-  const {password} = req.body;
+const resetPassword = asyncHandler(async (req, res) => {
+  const { password } = req.body;
   const token = req.params.token;
   const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
   const user = await User.findOne({
     passwordResetToken: hashedToken,
-    passwordResetExpires: {$gt: Date.now()}
+    passwordResetExpires: { $gt: Date.now() },
   });
-  if(!user){
+  if (!user) {
     throw new Error("Token Expired. Please try again later");
   }
   user.password = password;
@@ -255,13 +329,23 @@ const resetPassword = asyncHandler(async(req, res) =>{
   user.passwordResetExpires = undefined;
 
   await user.save();
-  res.json({user:user, message: "Password reset successful" });
+  res.json({ user: user, message: "Password reset successful" });
+});
+
+const getWishList = asyncHandler(async(req, res) =>{
+  const {_id} = req.user;
+  try{
+  const findUser = await User.findById(_id).populate("wishlist")
+  res.json(findUser)
+  } catch(error){
+    throw new Error(error)
+  }
 })
 
 
 module.exports = {
   createUser,
-  loginUserController,
+  loginUser,
   getAllUsers,
   getUser,
   deleteUser,
@@ -272,5 +356,8 @@ module.exports = {
   logout,
   updatePassword,
   forgotPasswordToken,
-  resetPassword
+  resetPassword,
+  loginAdmin,
+  getWishList,
+  updateInfo,
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { authMiddleware, isAdmin } = require("../middleware/authMiddleWare");
 const {
   createUser,
-  loginUserController,
+  loginUser,
   getAllUsers,
   getUser,
   deleteUser,
@@ -16,33 +16,31 @@ const {
   updatePassword,
   forgotPasswordToken,
   resetPassword,
+  loginAdmin,
+  getWishList,
+  updateInfo
 } = require("../controller/userController");
 
-// Auth 
+
 router.post("/register", createUser);
 router.post('/forgot-password-token', forgotPasswordToken );
 router.put('/reset-password/:token', resetPassword );
 
 router.put('/password',authMiddleware, updatePassword);
-router.get("/login", loginUserController);
+router.post("/login", loginUser);
+router.post("/admin-login", loginAdmin);
 router.get("/refresh", handleRefreshToken);
 router.get("/logout", logout);
+router.get("/wishlist", authMiddleware, getWishList);
 
-// User actions
 router.get("/all-users", getAllUsers);
 router.get("/:id",authMiddleware,isAdmin,getUser);
 router.delete("/:id", deleteUser);
 router.put("/update-user",authMiddleware,isAdmin, updateUser);
+router.put("/update-informaion", authMiddleware, updateInfo)
 
-// Admin actions
 router.put("/block-user/:id", authMiddleware, blockUser);
 router.put("/unlock-user/:id", authMiddleware, unlockUser);
-
-
-
-
-
-
 
 
 
