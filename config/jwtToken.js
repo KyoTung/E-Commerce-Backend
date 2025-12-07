e882e2c5ffe70b044414cb8a678913af2ee01ac9
KyoTung <144534270+KyoTung@users.noreply.github.com>
2025-12-08 00:02:00 +0700

    fix: sua logic dang nhap voi token

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -1,10 +1,22 @@
-const jwt = require("jsonwebtoken")
-const dotenv = require("dotenv");
-dotenv.config();
 
+// utils/jwt.js
+const jwt = require('jsonwebtoken');
 
-const generateToken = (id) =>{
-  return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: "1d"});
-}
+const generateAccessToken = (user) => {
+  // payload tối giản để giảm rủi ro
+  return jwt.sign(
+    { sub: user._id.toString(), role: user.role }, // sub = subject
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: '1h' } 
+  );
+};
 
-module.exports = { generateToken };
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { sub: user._id.toString() },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: '30d' } 
+  );
+};
+
+module.exports = { generateAccessToken, generateRefreshToken };
--- config/refreshToken.js
+++ config/refreshToken.js
@@ -3,8 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 
-const generateRefreshToken = (id) =>{
-  return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: "3d"});
-}
-
+// const generateRefreshToken = (id) =>{
+//   return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, {expiresIn: "30d"});
+// }
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { sub: user._id.toString() },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: '30d' } 
+  );
+};
 module.exports = { generateRefreshToken };
