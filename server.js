6aa2f2cdde8991288d99b2260848d1dc99eee305
KyoTung <144534270+KyoTung@users.noreply.github.com>
2026-01-07 22:45:05 +0700

    feat: them chuc nang login bang google

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const cookieParser = require("cookie-parser");
-const morgan = require("morgan")
-
+const morgan = require("morgan");
+const passport = require("passport");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -17,23 +17,26 @@ const categoryRoute = require("./routes/categoryRoute");
 const brandRoute = require("./routes/brandRoute");
 const blogcateRoute = require("./routes/blogcateRoute");
 const couponRoute = require("./routes/couponRoute");
-const orderRoute = require("./routes/orderRoute")
-const cartRoute = require("./routes/cartRoute")
-const colorRoute = require("./routes/colorRoute")
-const enquiryRoute = require("./routes/enquiryRoute")
+const orderRoute = require("./routes/orderRoute");
+const cartRoute = require("./routes/cartRoute");
+const colorRoute = require("./routes/colorRoute");
+const enquiryRoute = require("./routes/enquiryRoute");
+require("./config/passport");
 
 // Middleware
-app.use(morgan("dev"))
-const CLIENT_ORIGIN = 'http://localhost:3000'; // React dev
+app.use(morgan("dev"));
+const CLIENT_ORIGIN = "http://localhost:3000"; // React dev
+
+app.use(passport.initialize());
 
 // CORS cho tất cả route
 app.use(
   cors({
-    origin: CLIENT_ORIGIN,        
-    credentials: true,            
-    methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type','Authorization'],
-    exposedHeaders: [],            
+    origin: CLIENT_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: [],
   })
 );
 app.use(express.json());
@@ -43,14 +46,14 @@ app.use(cookieParser());
 connectDB();
 
 //api de ping den render tranh sleep time
-app.get('/ping', (req, res) => {
-    return res.send('Pong');
+app.get("/ping", (req, res) => {
+  return res.send("Pong");
 });
 
 // Routes
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
-app.use("/api/blog",blogRouter );
+app.use("/api/blog", blogRouter);
 app.use("/api/category", categoryRoute);
 app.use("/api/brand", brandRoute);
 app.use("/api/blogcategory", blogcateRoute);
@@ -59,10 +62,6 @@ app.use("/api/cart", cartRoute);
 app.use("/api/order", orderRoute);
 app.use("/api/color", colorRoute);
 app.use("/api/enquiry", enquiryRoute);
-
-
-
-
 
 app.use(notFound);
 app.use(errorHandler);
