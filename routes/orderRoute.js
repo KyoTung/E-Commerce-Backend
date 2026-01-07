62cbe952955caa94a97ed939e553f38184a76331
KyoTung <144534270+KyoTung@users.noreply.github.com>
2026-01-07 23:05:13 +0700

    feat: them tinh nang huy don hang tu phia user

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,13 +2,14 @@ const express = require("express");
 const { isAdmin, authMiddleware } = require("../middleware/authMiddleWare");
 
 const router = express.Router();
-const {createOrder, getOrderUser, updateStatus, getAllOrders, getOrderDetail} = require("../controller/orderController")
+const {createOrder, getOrderUser, updateStatus, getAllOrders, getOrderDetail, cancelOrder} = require("../controller/orderController")
 
 
 router.post("/", authMiddleware, createOrder);
 router.get("/user-orders", authMiddleware, getOrderUser);
 router.get("/order-detail/:id",authMiddleware, getOrderDetail)
 router.put("/:id",authMiddleware, isAdmin,updateStatus )
+router.put("/cancel-order/:id",authMiddleware,cancelOrder )
 router.get("/", authMiddleware, isAdmin, getAllOrders);
 
 module.exports = router;
