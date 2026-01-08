3a1d467d63b7c0a48813e278db78f7a6dd549bef
KyoTung <144534270+KyoTung@users.noreply.github.com>
2026-01-08 22:12:01 +0700

    lau url front end tu env

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://e-commerce-backend-f62w.onrender.com/api/user/auth/google/callback",
+      callbackURL: `${process.env.SERVER_URL}/api/user/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
